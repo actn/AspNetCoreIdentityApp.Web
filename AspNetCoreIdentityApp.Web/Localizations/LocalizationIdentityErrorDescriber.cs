f1bae6e38796630335f26791953fa00692f4a12d
actn <anilcetin@anadolu.edu.tr>
2023-10-17 22:01:57 +0300

    error message localizations

---
+++ AspNetCoreIdentityApp.Web/Localizations/LocalizationIdentityErrorDescriber.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreIdentityApp.Web.Localizations
+{
+    public class LocalizationIdentityErrorDescriber:IdentityErrorDescriber
+    {
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new() { Code = nameof(DuplicateUserName), Description = $"{userName} kullanıcı adı daha önce alınmış." };
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new() { Code = nameof(DuplicateEmail), Description = $"{email} mail adresi daha önce kullanılmış." };
+        }
+
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new() { Code = nameof(PasswordTooShort), Description = $"Şifre için {length} uzunluğu yeterli değil. En az 6 karakterden oluşmalı. " };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new() { Code = nameof(PasswordRequiresLower), Description = $"Şifre küçük harf içermeli. " };
+        }
+    }
+}
